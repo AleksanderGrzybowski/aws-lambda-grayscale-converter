99741aba9a5ebaf8f018705c9d9a05f087619dc7
Aleksander Grzybowski <me@aleksandergrzybowski.pl>
2024-02-17 18:45:23 +0100

    Make it work with new Terraform

--- main.tf
+++ main.tf
@@ -26,9 +26,9 @@ resource "aws_lambda_function" "grayscale" {
   filename      = "build/distributions/aws-lambda-grayscale-converter.zip"
   function_name = "convert-to-grayscale"
   handler       = "pl.kelog.ConvertToGrayscaleHandler::handleRequest"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  role          = aws_iam_role.iam_for_lambda.arn
   runtime       = "java8"
   timeout       = 30
 
-  source_code_hash = "${base64sha256(file("build/distributions/aws-lambda-grayscale-converter.zip"))}"
+  source_code_hash = filebase64sha256("build/distributions/aws-lambda-grayscale-converter.zip")
 }
